f1fccb62d0d6f86475b65d39ad29b42207b484d0
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-07-07 14:10:09 +0530

    update events animation

--- app/components/Event.jsx
+++ app/components/Event.jsx
@@ -7,9 +7,9 @@ const Event = ({ heading, paragraph, image, date, index }) => {
   return (
     <motion.div
       className={styles.container}
-      initial={{ y: "50%", opacity: 0 }}
+      initial={{ y: "20%", opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
-      transition={{ delay: 0.05 * index }}
+      transition={{ delay: 0.04 * index }}
       viewport={{ once: true }}
     >
       <div
